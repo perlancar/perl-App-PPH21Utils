4d0e91e0fedc93020083f9d754397996361d9322
perlancar (@pc-office) <perlancar@gmail.com>
2015-12-22 23:44:51 +0700

    Initial commit

---
+++ lib/App/PPH21Utils.pm
@@ -0,0 +1,18 @@
+package App::PPH21Utils;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: CLI utilities related to Indonesian income tax article 21 ("PPH21")
+
+=head1 DESCRIPTION
+
+This distribution includes the following CLI utilities:
+
+#INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<Business::Tax::ID::PPH21>
